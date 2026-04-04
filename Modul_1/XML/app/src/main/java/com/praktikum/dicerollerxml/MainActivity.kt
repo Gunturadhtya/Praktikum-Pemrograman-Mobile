60a1e3a27d2e2286259715fb136ad69e935f4020
Guntur <gunturpdng@gmail.com>
2026-04-04 16:58:45 +0800

    feat: refactor XML logic into more functional and coupled
    Signed-off-by: Guntur <gunturpdng@gmail.com>

--- Modul_1/XML/app/src/main/java/com/praktikum/dicerollerxml/MainActivity.kt
+++ Modul_1/XML/app/src/main/java/com/praktikum/dicerollerxml/MainActivity.kt
@@ -10,44 +10,43 @@ import com.praktikum.dicerollerxml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val diceResults = mutableListOf(1, 3)
+    private var diceResults = listOf(1, 3)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        updateUi()
-
+        updateUi(diceResults)
         binding.rollButton.setOnClickListener {
-            rollDice()
+            diceResults = rollDice(2)
+            updateUi(diceResults)
+            Toast.makeText(this, getString(checkLuck(diceResults)), Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun rollDice() {
-
-        diceResults[0] = (0..5).random()
-        diceResults[1] = (0..5).random()
-        updateUi()
-
-        if(diceResults[0] == diceResults[1]) Toast.makeText(this, getString(R.string.lucky), Toast.LENGTH_SHORT).show()
-        else Toast.makeText(this, getString(R.string.badluck), Toast.LENGTH_SHORT).show()
-    }
-
-    private fun updateUi() {
+    private fun updateUi(results : List<Int>) {
         val views = listOf(binding.diceImage1, binding.diceImage2)
 
-        diceResults.forEachIndexed { index, result ->
-            val drawableRes = when (result) {
-                0 -> R.drawable.dice_1
-                1 -> R.drawable.dice_2
-                2 -> R.drawable.dice_3
-                3 -> R.drawable.dice_4
-                4 -> R.drawable.dice_5
-                else -> R.drawable.dice_6
-            }
-            views[index].setImageResource(drawableRes)
-            views[index].contentDescription = result.toString()
+        views.zip(results).forEach { (view, result) ->
+            view.setImageResource(getDrawableDice(result))
+            view.contentDescription = result.toString()
         }
     }
+
+    fun getDrawableDice(value: Int): Int = when (value) {
+        0 -> R.drawable.dice_1
+        1 -> R.drawable.dice_2
+        2 -> R.drawable.dice_3
+        3 -> R.drawable.dice_4
+        4 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    fun rollDice(count: Int = 2): List<Int> = List(count) {(1..6).random()}
+
+    fun checkLuck(results: List<Int>): Int =
+        if (results.all { it == results.first() }) R.string.lucky
+        else R.string.badluck
+
+
 }
