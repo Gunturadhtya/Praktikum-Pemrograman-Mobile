9a1315eb538c6795e16ceb88b054c44fcb8bb086
Guntur <gunturpdng@gmail.com>
2026-04-04 18:33:02 +0800

    feat: add dynamic dice based on diceResult
    Signed-off-by: Guntur <gunturpdng@gmail.com>

--- Modul_1/XML/app/src/main/java/com/praktikum/dicerollerxml/MainActivity.kt
+++ Modul_1/XML/app/src/main/java/com/praktikum/dicerollerxml/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.praktikum.dicerollerxml
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -10,7 +12,7 @@ import com.praktikum.dicerollerxml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var diceResults = listOf(1, 3)
+    private var diceResults = listOf(0, 0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,27 +20,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         updateUi(diceResults)
         binding.rollButton.setOnClickListener {
-            diceResults = rollDice(2)
+            diceResults = rollDice(diceResults.size)
             updateUi(diceResults)
             Toast.makeText(this, getString(checkLuck(diceResults)), Toast.LENGTH_SHORT).show()
         }
     }
 
     private fun updateUi(results : List<Int>) {
-        val views = listOf(binding.diceImage1, binding.diceImage2)
-
-        views.zip(results).forEach { (view, result) ->
-            view.setImageResource(getDrawableDice(result))
-            view.contentDescription = result.toString()
+        binding.diceContainer.removeAllViews()
+        results.forEach { result ->
+            val diceView = ImageView(this).apply {
+                layoutParams = LinearLayout.LayoutParams(
+                    0,
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    1.0f
+                ).apply {
+                    setMargins(8,8,8,8)
+                }
+                setImageResource(getDrawableDice(result))
+                contentDescription = result.toString()
+            }
+            binding.diceContainer.addView(diceView)
         }
     }
 
     fun getDrawableDice(value: Int): Int = when (value) {
-        0 -> R.drawable.dice_1
-        1 -> R.drawable.dice_2
-        2 -> R.drawable.dice_3
-        3 -> R.drawable.dice_4
-        4 -> R.drawable.dice_5
+        0 -> R.drawable.dice_0
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
 
