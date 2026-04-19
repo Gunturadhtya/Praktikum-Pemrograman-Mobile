b9551ec6f4d29dcde71666239a13afc21eb933cb
guntur <gunturpdng@gmail.com>
2026-04-19 12:33:14 +0000

    fix (modul 2 compose): directly return the value on calculatetip()

--- Modul_2/Compose/app/src/main/java/com/example/modul2/TipCalculatorModel.kt
+++ Modul_2/Compose/app/src/main/java/com/example/modul2/TipCalculatorModel.kt
@@ -10,11 +10,10 @@ object TipCalculatorModel {
         isRoundUp: Boolean
     ): BigDecimal {
         val calculatedTip = amount.multiply(percentage)
-        val finalTip = if (isRoundUp) {
+        return if (isRoundUp) {
             calculatedTip.setScale(0, RoundingMode.UP).setScale(2)
         } else {
             calculatedTip.setScale(2, RoundingMode.HALF_EVEN)
         }
-        return finalTip
     }
 }
