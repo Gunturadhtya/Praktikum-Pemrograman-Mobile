5e9dba5ede12e76cbfca358288572c43e5b1aa76
guntur <gunturpdng@gmail.com>
2026-04-19 02:59:45 +0000

    feat: basic layout

--- Modul_2/Compose/app/src/main/java/com/example/modul2/MainActivity.kt
+++ Modul_2/Compose/app/src/main/java/com/example/modul2/MainActivity.kt
@@ -1,17 +1,46 @@
 package com.example.modul2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.modul2.ui.theme.Modul2Theme
+import java.math.BigDecimal
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +48,102 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Modul2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    TipCalculator()
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TipCalculator(modifier: Modifier = Modifier) {
+    val options = listOf("15", "18", "20")
+    var expanded by remember { mutableStateOf(false) }
+    var selectedOption by remember { mutableStateOf("15") }
+    var checked by remember { mutableStateOf(true) }
+    val billAmountState = rememberTextFieldState()
+    var tipAmount by remember { mutableStateOf(BigDecimal(0)) }
+    var isVisibile by remember { mutableStateOf(true) }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Calculate Tip"
+        )
+
+        TextField(
+            state = billAmountState,
+            lineLimits = TextFieldLineLimits.SingleLine,
+            placeholder = { Text("Bill Amount") }
+        )
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
+        ) {
+
+            TextField(
+                value = selectedOption,
+                onValueChange = {},
+                readOnly = true,
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                },
+                colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                suffix = {Text("%")},
+                modifier = Modifier
+                    .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryNotEditable)
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                options.forEach { option ->
+                    DropdownMenuItem(
+                        text = { Text(option, color = MaterialTheme.colorScheme.onSurface) },
+                        onClick = {
+                            selectedOption = option
+                            expanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                    )
+                }
+            }
+        }
+
+        Row(){
+            Text(
+                text = "Round up tip?"
+            )
+            Switch(
+                checked = checked,
+                onCheckedChange = {
+                    checked = it
+                    isVisibile = it
+                }
+            )
+        }
+
+        Text(
+            text = "Tip Amount : $$tipAmount",
+            modifier = Modifier.alpha(if(isVisibile) 1f else 0f)
+        )
+    }
+}
+
+fun calculateTip(bill : BigDecimal, tip : BigDecimal) : BigDecimal{
+    val out = bill * (tip/100.toBigDecimal());
+    Log.d("result of calculate tip : ", out.toString())
+    return bill * (tip/100.toBigDecimal());
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Modul2Theme {
-        Greeting("Android")
+        TipCalculator()
     }
 }
