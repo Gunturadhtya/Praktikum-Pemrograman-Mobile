48dc524f395acd18c53fbe1b90676913a7f59f11
guntur <gunturpdng@gmail.com>
2026-04-30 03:06:29 +0000

    feat (modul 3): change button language to icon

--- Modul_3/Compose/app/src/main/java/com/mobil/modul3compose/HomeScreen.kt
+++ Modul_3/Compose/app/src/main/java/com/mobil/modul3compose/HomeScreen.kt
@@ -17,10 +17,14 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Language
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
@@ -51,12 +55,21 @@ fun ProblemScreen(dummyProblemList: List<CodeforcesProblem>, navController: NavC
         modifier = modifier.fillMaxSize()
     ) {
         item{
-            Button(
-                onClick = {
-                    navController.navigate(RoutingNames.LanguageScreen)
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                ,horizontalArrangement = Arrangement.End
+            ) {
+                IconButton(
+                    onClick = {
+                        navController.navigate(RoutingNames.LanguageScreen)
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Language,
+                        contentDescription = "Change Language"
+                    )
                 }
-            ){
-                Text("*")
             }
         }
         item {
