db953f59a85cb70fbeff02a6e0b88afbde2a92f7
guntur <gunturpdng@gmail.com>
2026-06-07 17:29:55 +0800

    feat(modul-5-compose): update BaseRepository.kt and MovieRepository.kt to return Flow

--- Modul_5/Compose/app/src/main/java/com/mobil/modul5compose/network/BaseRepository.kt
+++ Modul_5/Compose/app/src/main/java/com/mobil/modul5compose/network/BaseRepository.kt
@@ -4,23 +4,29 @@ import io.ktor.client.call.body
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.isSuccess
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import timber.log.Timber
 
 abstract class BaseRepository {
-    protected suspend inline fun <reified T> safeApiCall(
-        call: () -> HttpResponse
-    ): Result<T> = runCatching {
-        val response = call()
-        if (response.status.isSuccess()) {
-            response.body<T>()
-        } else {
-            val errorBody = response.bodyAsText()
-            val errorMessage = "Status: ${response.status.value} | Body: $errorBody"
+    protected suspend inline fun <reified T> safeApiFlow(
+        crossinline call: suspend () -> HttpResponse
+    ): Flow<Result<T>> = flow<Result<T>> {
+        val result = runCatching {
+            val response = call()
+            if (response.status.isSuccess()) {
+                response.body<T>()
+            } else {
+                val errorBody = response.bodyAsText()
+                val errorMessage = "Status: ${response.status.value} | Body: $errorBody"
 
-            Timber.tag("Network").e("API Failure: $errorMessage")
-            throw Exception("API Error: ${response.status.value}")
+                Timber.tag("Network").e("API Failure: $errorMessage")
+                throw Exception("API Error: ${response.status.value}")
+            }
+        }.onFailure { exception ->
+            Timber.tag("Network").e(exception, "Network Failure")
         }
-    }.onFailure { exception ->
-        Timber.tag("Network").e(exception, "Network Failure")
-    }
+    }.flowOn(Dispatchers.IO)
 }
--- Modul_5/Compose/app/src/main/java/com/mobil/modul5compose/network/MovieRepository.kt
+++ Modul_5/Compose/app/src/main/java/com/mobil/modul5compose/network/MovieRepository.kt
@@ -3,10 +3,11 @@ package com.mobil.modul5compose.network
 import com.mobil.modul5compose.data.MovieResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.request.get
+import kotlinx.coroutines.flow.Flow
 
 class MovieRepository(private val client: HttpClient) : BaseRepository() {
-    suspend fun getMovie(movieId: Int): Result<MovieResponse> {
-        return safeApiCall {
+    suspend fun getMovie(movieId: Int): Flow<Result<MovieResponse>> {
+        return safeApiFlow {
             client.get("movie/$movieId?language=en-US")
         }
     }
