a2ca42cf6b20ba977676efb1cea01c5dbaf9e282
guntur <gunturpdng@gmail.com>
2026-06-07 17:55:58 +0800

    feat(modul-5-compose): add movie entity

---
+++ Modul_5/Compose/app/src/main/java/com/mobil/modul5compose/data/local/MovieEntity.kt
@@ -0,0 +1,16 @@
+package com.mobil.modul5compose.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movies")
+data class MovieEntity(
+    @PrimaryKey
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val posterPath: String,
+    val releaseDate: String,
+    val voteAverage: Double,
+    val cachedAt: Long = System.currentTimeMillis()
+)
