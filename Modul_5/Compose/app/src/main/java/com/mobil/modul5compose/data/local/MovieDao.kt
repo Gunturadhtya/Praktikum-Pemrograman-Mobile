e96bd5296c26301783beb82890a1d1753016c1c9
guntur <gunturpdng@gmail.com>
2026-06-07 18:03:17 +0800

    feat(modul-5-compose): add movie DAO

---
+++ Modul_5/Compose/app/src/main/java/com/mobil/modul5compose/data/local/MovieDao.kt
@@ -0,0 +1,25 @@
+package com.mobil.modul5compose.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MovieDao {
+    @Query("SELECT * FROM movies WHERE id = :movieId")
+    fun getMovieId(movieId: Int): Flow<MovieEntity>
+
+    @Query("SELECT * FROM movies ORDER BY cachedAt DESC")
+    fun getAllMovies(): Flow<List<MovieEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovie(movie: MovieEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovie(movie: List<MovieEntity>)
+
+    @Query("DELETE FROM movies")
+    suspend fun clearAllMovies()
+}
